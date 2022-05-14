68cf7897b1c0ccc24894e739e41f24bac859e4d1
Tarun-108 <tarunstu125@gmail.com>
2022-05-14 23:51:58 +0530

    family details response part 1

--- app/src/main/java/com/woc/jangarana/fragments/UserDetail2Fragment.java
+++ app/src/main/java/com/woc/jangarana/fragments/UserDetail2Fragment.java
@@ -55,7 +55,7 @@ public class UserDetail2Fragment extends Fragment {
                 detailsModel.setMaritalStatus(false);
 //                detailsModel.setAgeMarried(Integer.parseInt(binding.ageOfMarrige.getText().toString()));
                 detailsModel.setAgeMarried(26);
-                detailsModel.setReligion(binding.religion.getText().toString());
+                detailsModel.setReligion("hindu");
                 detailsModel.setMotherTongue("hindi");
                 detailsModel.setOtherTongue("english");
                 detailsModel.setDisabled(false);
