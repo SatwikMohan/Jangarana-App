7512230fdb4227b02ada0e361eef70b20f608328
Ansh <anshtandonat@gmail.com>
2022-09-03 12:50:34 +0530

    resolved a UI issue

--- app/src/main/java/com/woc/jangarana/staff/StaffDashboardActivity.java
+++ app/src/main/java/com/woc/jangarana/staff/StaffDashboardActivity.java
@@ -2,11 +2,14 @@ package com.woc.jangarana.staff;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.woc.jangarana.PersonWiseData;
 import com.woc.jangarana.R;
 import com.woc.jangarana.databinding.ActivityStaffDashboardBinding;
+import com.woc.jangarana.familyhead.DashboardActivity;
 import com.woc.jangarana.fragments.AddFirstFragment;
 import com.woc.jangarana.fragments.StaffDashboardFragment;
 import com.woc.jangarana.fragments.StaffProfileFragment;
@@ -21,7 +24,7 @@ public class StaffDashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityStaffDashboardBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_staff_dashboard);
+        setContentView(binding.getRoot());
 
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.flFragment, new StaffDashboardFragment(this))
@@ -48,10 +51,7 @@ public class StaffDashboardActivity extends AppCompatActivity {
                 binding.bottomNavProfile.setCardBackgroundColor(getResources().getColor(R.color.highlight_gray));
                 binding.bottomNavForm.setStrokeColor(getResources().getColor(R.color.white));
                 binding.bottomNavForm.setCardBackgroundColor(getResources().getColor(R.color.white));
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.flFragment,
-                                new StaffProfileFragment())
-                        .commit();
+                startActivity(new Intent(StaffDashboardActivity.this, PersonWiseData.class));
             }
         });
 
